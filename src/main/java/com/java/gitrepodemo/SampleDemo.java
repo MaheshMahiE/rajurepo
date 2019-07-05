23fdfb0c4e9775fb0d3e7150c9e4195b07db30a6
mahesh <emahesh16@gmail.com>
2019-07-05 11:33:36 +0530

    initial commit

---
+++ src/main/java/com/java/gitrepodemo/SampleDemo.java
@@ -0,0 +1,15 @@
+package com.java.gitrepodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SampleDemo {
+
+	public static void main(String[] args) {
+	
+		ConfigurableApplicationContext context = SpringApplication.run(SampleDemo.class, args);
+		System.out.println("hello raju this is my 1st program..."+context);
+	}
+}
